1d5fde73ba214f099303b9794f570553b92e61f3
Arthur Kalikiti <arthurkalikiti@gmail.com>
2024-12-16 12:17:16 +0200

    feat: Added an Anvil cloud loader

---
+++ src/lib/components/NavigationLoader.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { navigating } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	export let animationDuration = 250;
+	export let color: string = '#7fffd4';
+	export let height = 4;
+
+	let currentState: 'waiting' | 'loading' | 'doneLoading' | 'cleanupLoading' = 'waiting';
+
+	let resetTimeout: number | undefined = undefined;
+
+	onMount(() => {
+		navigating.subscribe((n) => {
+			if (n) {
+				currentState = 'loading';
+			} else {
+				if (currentState == 'loading') {
+					currentState = 'doneLoading';
+
+					if (resetTimeout) {
+						clearTimeout(resetTimeout);
+						resetTimeout = undefined;
+					}
+
+					// Start cleanup animation
+					resetTimeout = window.setTimeout(() => {
+						currentState = 'cleanupLoading';
+						resetTimeout = window.setTimeout(() => {
+							currentState = 'waiting';
+						}, animationDuration);
+					}, animationDuration);
+				}
+			}
+		});
+	});
+</script>
+
+<div class="navigation-loader-container" style="--height: {height}px">
+	<div
+		class="navigation-loader-loading-bar {currentState}"
+		style="background-color: {color}; --duration: {animationDuration}ms">
+    </div>
+</div>
+
+<style>
+	.navigation-loader-container {
+		z-index: 99999;
+		pointer-events: none;
+		position: fixed;
+		top: 0;
+		left: 0;
+		min-width: 100vw;
+		width: 100%;
+		height: var(--height);
+
+		.navigation-loader-loading-bar {
+			display: block;
+			height: 100%;
+			transition: opacity ease-in-out var(--duration);
+
+			&.waiting {
+				width: 0;
+				opacity: 0;
+			}
+
+			&.loading {
+				transition: width cubic-bezier(0, 0.55, 0.45, 1) 20s;
+				width: 50%;
+				opacity: 1;
+			}
+
+			&.doneLoading {
+				transition: width ease-in-out var(--duration);
+				width: 100%;
+				opacity: 1;
+			}
+
+			&.cleanupLoading {
+				transition: opacity ease-in-out var(--duration);
+				opacity: 0;
+				width: 100%;
+			}
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
     import '../app.css'
+	import NavigationLoader from '$lib/components/NavigationLoader.svelte';
     let { children } = $props();
 </script>
 
 <ModeWatcher />
+<NavigationLoader 
+	animationDuration={250} 
+	color="#D0D0D0" 
+	height={3} 
+/>
 {@render children?.()}
