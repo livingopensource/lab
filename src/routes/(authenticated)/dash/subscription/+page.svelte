239e989ac3d605a2b5202f334281c51b8d718fcb
Arthur Kalikiti <arthurkalikiti@gmail.com>
2024-12-24 01:50:18 +0200

    feat: Added some text on empty pages

--- src/routes/(authenticated)/dash/subscription/+page.svelte
+++ src/routes/(authenticated)/dash/subscription/+page.svelte
@@ -2,6 +2,10 @@
     <title>Subscription | SwiftCloud Labs</title>
 </svelte:head>
 
-<div class="container">
-
+<div class="container content-center w-screen h-screen">
+    <div>
+    </div>
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="text-9xl">Imagination in progress</h1>
+    </div>
 </div>
