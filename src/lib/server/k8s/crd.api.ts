dc373a0da477a0913e67b9ea71ff3816824fc785
Arthur Kalikiti <arthurkalikiti@gmail.com>
2025-01-13 22:15:20 +0200

    feat: Power on/off implemenatation

--- src/lib/server/k8s/crd.api.ts
+++ src/lib/server/k8s/crd.api.ts
@@ -1,4 +1,9 @@
 import { k8sCrdApi } from '.';
+import * as k8s from '@kubernetes/client-node'
+const kc = new k8s.KubeConfig()
+kc.loadFromDefault()
+
+import { PromiseMiddlewareWrapper } from '@kubernetes/client-node/dist/gen/middleware';
 
 export async function virtualMachines(project: string) {
 	const virtualMachines = await k8sCrdApi.listNamespacedCustomObject({
@@ -23,18 +28,46 @@ export async function virtualMachine(name: string, project: string) {
 
 export async function virtualMachinePowerToggle(name: string, project: string, strategy: string) {
 	try {
+		const patch = [{
+			op: "replace",
+			path: "/spec/runStrategy",
+			value: strategy
+		}]
+		const headerPatchMiddleware = new PromiseMiddlewareWrapper({
+			pre: async(requestContext) => {
+				requestContext.setHeaderParam("Content-type", "application/json-patch+json");
+				return requestContext;
+			},
+			post: async (responseContext) => responseContext,
+		});
+	
+		const currentContext = kc.getCurrentContext()
+		const currentCluster = kc.getCluster(currentContext)
+		if (currentCluster === undefined || currentCluster === null) {
+			throw new Error("cluster is undefined")
+		}
+		const server = currentCluster.server;
+		if (server === undefined) {
+			throw new Error("server is undefined")
+		}
+		
+		const baseServerConfig = new k8s.ServerConfiguration(server, {})
+		const configuration = k8s.createConfiguration({
+			middleware: [headerPatchMiddleware],
+			baseServer: baseServerConfig,
+			authMethods: {
+				default: kc
+			}
+		})
+
 		const virtualMachine = await k8sCrdApi.patchNamespacedCustomObject({
 			group: 'kubevirt.io',
 			version: 'v1',
 			namespace: project,
 			plural: 'virtualmachines',
 			name: name,
-			body: {
-				spec: {
-					runStrategy: strategy
-				}
-			}
-		});
+			body: patch
+		}, configuration);
 		return virtualMachine;
 	} catch (err: unknown) {
 		if (err instanceof Error) {
