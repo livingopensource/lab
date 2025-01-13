f33b6dd0be1fd5158cb5dd3d16f4425a4cbb77d5
Arthur Kalikiti <arthurkalikiti@gmail.com>
2025-01-13 18:07:26 +0200

    wip: Added power toggle for vm instances
    The iimplemenation is failing due to malfoed body payload

--- src/lib/server/k8s/crd.api.ts
+++ src/lib/server/k8s/crd.api.ts
@@ -21,6 +21,29 @@ export async function virtualMachine(name: string, project: string) {
 	return virtualMachine;
 }
 
+export async function virtualMachinePowerToggle(name: string, project: string, strategy: string) {
+	try {
+		const virtualMachine = await k8sCrdApi.patchNamespacedCustomObject({
+			group: 'kubevirt.io',
+			version: 'v1',
+			namespace: project,
+			plural: 'virtualmachines',
+			name: name,
+			body: {
+				spec: {
+					runStrategy: strategy
+				}
+			}
+		});
+		return virtualMachine;
+	} catch (err: unknown) {
+		if (err instanceof Error) {
+			return err.message;
+		}
+		return 'An unknown error occurred';
+	}
+}
+
 export async function deleteVirtualMachine(name: string, project: string) {
 	try {
 		const virtualMachine = await k8sCrdApi.deleteNamespacedCustomObject({
--- src/routes/(authenticated)/api/[name]/state/+server.ts
+++ src/routes/(authenticated)/api/[name]/state/+server.ts
@@ -1,9 +1,6 @@
 import { json } from '@sveltejs/kit';
-import {fetch, Agent} from 'undici';
-import { env } from '$env/dynamic/private';
-import * as fs from 'node:fs';
-import type { operationResponse } from '$lib/server/incus.types';
 import { hash } from '$lib/server/utils.js';
+import { virtualMachinePowerToggle } from '$lib/server/k8s/crd.api.js';
 
 export const PUT = async ({locals, url, params}) => {
     const session = await locals.auth()
@@ -13,37 +10,27 @@ export const PUT = async ({locals, url, params}) => {
     const project = hash(session?.user?.email ?? "") ?? "none";
     const state = url.searchParams.get("state")
     if (state != null) {
-        const res = await fetch(`${env.CLUSTER_URL}/1.0/instances/${params.name}/state?project=${project}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            dispatcher: new Agent({
-                connect: {
-                    cert: fs.readFileSync(env.CERT),
-                    key: fs.readFileSync(env.KEY),
-                    rejectUnauthorized: false,
-                }
-            }),
-            body: JSON.stringify({
-                action: state,
-                timeout: 10,
-                force: false,
-                stateful: false,
-            })
-        })
-    
-        if (!res.ok) {
-            return json({"error": res.statusText}, {
-                status: 400
-            })
+
+        if (state == "start") {
+            const vm = await virtualMachinePowerToggle(params.name, project, "Always")
+            console.log(vm)
+            if (!vm.ok) {
+                return json({"error": `unable to ${state} instance`}, {
+                    status: 400
+                })
+            }
+            return new Response(String(vm))
+        } else if (state == "stop") {
+            const vm = await virtualMachinePowerToggle(params.name, project, "Halted")
+            console.log(vm)
+            if (!vm.ok) {
+                return json({"error": `unable to ${state} instance`}, {
+                    status: 400
+                })
+            }
+            return new Response(String(vm))
         }
-    
-        const data = await res.json() as operationResponse
-        if (data.status_code == 100) {
-            return new Response(String(data))
-        }
-    
+
         return json({"error": `unable to ${state} instance`}, {
             status: 400
         })
