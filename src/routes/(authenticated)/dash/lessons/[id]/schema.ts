29f2b0aa2074371eb17e3eec35b9a69e73ffa0fc
Arthur Kalikiti <arthurkalikiti@gmail.com>
2025-06-09 15:33:54 +0200

    feat: Updated the content size to 15000 characters

--- src/routes/(authenticated)/dash/lessons/[id]/schema.ts
+++ src/routes/(authenticated)/dash/lessons/[id]/schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
  
 export const formSchema = z.object({
  title: z.string().min(3, {message: "only aphanumeric, underscores and hyphens are valid characters"}).min(2, {message: "Instance name must contain at least two characters"}).max(50),
- contents: z.string().min(10, {message: "Content must contain at least two characters"}).max(5000),
+ contents: z.string().min(10, {message: "Content must contain at least two characters"}).max(15000),
 });
  
 export type FormSchema = typeof formSchema;
